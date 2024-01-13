fcb8bcf1c3f439bbfc37d12742a54b1ab73f62ed
Jannik Völker <ampullen-raver0p@icloud.com>
2024-01-13 23:10:21 +0100

    connected front and backend

--- backend/use_spacy_v2.py
+++ backend/use_spacy_v2.py
@@ -1,36 +1,40 @@
 import spacy
 import json
 
+
 def use_spacy_v2(sentence):
     # Load trained model
-    nlp = spacy.load("/Users/nikyakovlev/nlp_project/backend/model")
-    
-    #nlp = spacy.load("../backend/model")
+    nlp = spacy.load("./model")
 
     # Process the sentence
     doc = nlp(sentence)
 
     # Extract PII entities and their positions
-    pii_entities = [(ent.start_char, ent.end_char, ent.label_) for ent in doc.ents]
+    pii_entities = [
+        (ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents
+    ]
 
     # Check if any PII entities were detected
     if pii_entities:
-        output = [(sentence, {'entities': pii_entities})]
-        json_output = json.dumps(output, indent=4)
-        print(json_output)
+        return pii_entities
+        # output = [(sentence, {"entities": pii_entities})]
+        # json_output = json.dumps(output, indent=4)
+        # print(json_output)
     else:
-        print("No PII entities detected.")
+        return None
+        # print("No PII entities detected.")
+
 
 # Example usage
 # Specify the path to the JSON file you want to import
 file_path = "input_data.json"
 
 # Open and read the JSON file
-with open(file_path, 'r') as json_file:
+with open(file_path, "r") as json_file:
     imported_data = json.load(json_file)
 
 # Access the imported data
-input_text = imported_data.get('input_text', '')
+input_text = imported_data.get("input_text", "")
 
-#function
+# function
 use_spacy_v2(input_text)
--- backend/use_spacy_v2_cl.py
+++ backend/use_spacy_v2_cl.py
@@ -1,11 +1,10 @@
 import spacy
 import json
 
+
 def use_spacy_v2_cl():
     # Load trained model
-    nlp = spacy.load("/Users/nikyakovlev/nlp_project/backend/model")
-
-    #nlp = spacy.load("../backend/model")
+    nlp = spacy.load("./model")
 
     # a sentence input from the command line
     sentence = input("Enter a sentence to analyze for PII: ")
@@ -14,15 +13,18 @@ def use_spacy_v2_cl():
     doc = nlp(sentence)
 
     # Extract PII entities and their positions
-    pii_entities = [(ent.start_char, ent.end_char, ent.label_) for ent in doc.ents]
+    pii_entities = [
+        (ent.text, ent.start_char, ent.end_char, ent.label_) for ent in doc.ents
+    ]
 
     # Check if any PII entities were detected
     if pii_entities:
-        output = [(sentence, {'entities': pii_entities})]
+        output = [(sentence, {"entities": pii_entities})]
         json_output = json.dumps(output, indent=4)
         print(json_output)
     else:
         print("No PII entities detected.")
 
-#function
+
+# function
 use_spacy_v2_cl()
